ab962ca8099a63cb39875046cd361a0a6b36bb25
Nguyễn Duy Việt <duyvietnguyen0805@gmail.com>
2024-12-20 22:08:52 +0700

    Sua file layout

---
+++ app/src/main/java/com/example/btthayhuynhomazure/Activity/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.example.btthayhuynhomazure.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.btthayhuynhomazure.R;
+import com.example.btthayhuynhomazure.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+    ActivitySignUpBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
